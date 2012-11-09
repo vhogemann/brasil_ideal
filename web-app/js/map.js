ea7ae5036cb5675c0691fb75aa9c196fc968d547
Juliano Fernandes <juliano.fernandes@ideais.com.br>
2012-11-09 21:03:45 -0200

    Identando js do mapa e alterando centralização padrão para a Ideais

--- web-app/js/map.js
+++ web-app/js/map.js
@@ -1,10 +1,11 @@
 function initialize() {
-var mapOptions = {
-  center: new google.maps.LatLng(-34.397, 150.644),
-  zoom: 8,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("map_canvas"),
-        mapOptions);
+	var mapOptions = {
+		center : new google.maps.LatLng(-22.909079507, -43.1770692),
+		zoom : 18,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }
+
 initialize();
