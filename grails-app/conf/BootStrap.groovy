4dcc098a174d80a5353f479ce1fff6aca664aebb
LucasBrum <lucasbrum7@gmail.com>
2012-11-09 21:09:11 -0200

    Comitando dados estáticos dos jogos no BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,22 @@
+import pubcup.*
+
 class BootStrap {
 
     def init = { servletContext ->
+
+    	Game.list()*.delete()
+
+    	def teams 		= TeamsEnum.values()
+    	def gameTeams	= []
+    	
+    	teams.eachWithIndex { team, i ->
+    		gameTeams << team
+    		if((i+1)%2==0) {
+    			new Game(date: new Date(), teams:gameTeams).save()
+    			gameTeams = []
+
+    		}
+    	}
     }
     def destroy = {
     }
