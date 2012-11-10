f18858ec20dccf5d03bfbc3a5d771c97c50f0194
Rodrigo Fournier <speakersk8@gmail.com>
2012-11-10 02:54:26 -0200

    Commitando arquivos javascript que faltaram

---
+++ web-app/js/index.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+	var url =  new String(window.location);
+
+	var urlparts = url.split("/");
+	var host = urlparts[0]+'//'+urlparts[2]+'/'+urlparts[3];
+	$('.searchKey').autocomplete(host+'/home/find', {
+		dataType: 'json',
+		parse: function(locations) {
+			return $.map(locations, function(location) {
+				return {
+					data: location,
+					value: location.name,
+					result: location.name
+				};
+			});
+		},
+		formatItem: function(location) {
+			return location.name;
+		}
+	}).result(function(event, location, formatted) {
+		
+	});
+});
