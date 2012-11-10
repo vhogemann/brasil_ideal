4c99eae0c42021a7068884db2a5caa9ee906a628
Bruno Menezes <brunodmenezes@gmail.com>
2012-11-10 06:07:53 -0200

    Adicionado plotLocations para busca e verificação de area ao redor

--- web-app/js/map.js
+++ web-app/js/map.js
@@ -1,6 +1,6 @@
 var map;
 var animation = false;
-var plotMapControl;
+var plotMapControl = {};
 
 function center( map ){
 	if( navigator.geolocation ){
@@ -28,43 +28,44 @@ function initialize() {
 	 
 
 window.setInterval(function() {
-    	
   	 	var bound = map.getBounds();
 		var lat1 = bound.ca.f;
 		var long1 = bound.ca.b;
 		var lat2 = bound.ea.f;
 		var long2 = bound.ea.b;
-		console.log(bound);
-		 	ajax({
-		  	 	url : config.contextPath + "/home/near/",
-		  	 	data : {lat1:lat1 , long1:long1 , lat2:lat2, long2:long2},
-		  	 	traditional : true,
-		  	 	success : findLocations
-		  	 });	
+		ajax({
+		  	 url : config.contextPath + "/home/near/",
+		  	 data : {lat1:lat1 , long1:long1 , lat2:lat2, long2:long2},
+		  	 traditional : true,
+		  	 success : plotLocations
+		 });	
 }, 5000);
 }
 
-function findLocations(locations){
+function plotLocations(locations){
 	var marker;
 	for(i=0 ; i<locations.length ;i++) {
-		marker = new google.maps.Marker({
-    	position: new google.maps.LatLng(locations[i].x,locations[i].y),
-    	map: map,
-    	title: locations[i].name,
-  	});
+	   if(!plotMapControl[hash]){
+	   	 var name = locations[i].name ;
+		 var lat = locations[i].lat;
+		 var lng = locations[i].lng;
+		 var hash = getKey(name,lat,lng);
+	   	 marker = new google.maps.Marker({
+    	 position: new google.maps.LatLng(lat, lng),
+    	 map: map,
+    	 title: name,
+    	 animation : google.maps.Animation.DROP
+  		});
+	   	 plotMapControl[hash]=locations[i];
+	   	 locations.length == 1 ? map.panTo(marker.getPosition()) : {};
+	   }
 	}	
 }
 
-
-function plotLocation(obj){
-	console.log(obj);
-	var marker = new google.maps.Marker({
-    	position: new google.maps.LatLng(obj.location[0],obj.location[1]),
-    	map: map,
-    	title: obj.name,
-  	});
-		map.panTo(marker.getPosition());
+function getKey(name,lat,lng){
+	return lat+" "+lng+" "+name;
 }
+
 
 function ajax(options){
 	$.ajax(options);
