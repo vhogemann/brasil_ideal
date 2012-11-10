375f871252408c6b3d518f4e0f9ab76e9a287eb6
Victor Hogemann <victor.hogemann@gmail.com>
2012-11-09 23:57:41 -0200

    limpando mapa

--- web-app/js/map.js
+++ web-app/js/map.js
@@ -12,7 +12,8 @@ function initialize() {
 	var mapOptions = {
 		center : new google.maps.LatLng(-22.909079507, -43.1770692),
 		zoom : 18,
-		mapTypeId : google.maps.MapTypeId.ROADMAP
+		mapTypeId : google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI : true
 	};
 
 	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
