af31e576a705ad22aa09c584217255b8ee2e6aed
Rodrigo Fournier <speakersk8@gmail.com>
2012-11-10 01:10:02 -0200

    Criação do método de busca no HomeController

--- grails-app/controllers/pubcup/HomeController.groovy
+++ grails-app/controllers/pubcup/HomeController.groovy
@@ -1,8 +1,17 @@
 package pubcup
 
+import grails.converters.JSON
+
 class HomeController {
 
     def index() {
 		
 	}
+
+	def find(String searchKey) {
+
+        def locations = Location.findAllByNameIlike("%${searchKey}%")
+
+        render locations as JSON
+    }
 }
