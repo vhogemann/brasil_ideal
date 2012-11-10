d9912a469e691e8915639aad383b3a7a3fe7fbc6
Victor Hogemann <victor.hogemann@gmail.com>
2012-11-10 02:46:19 -0200

    acertando query geoloc

--- grails-app/controllers/pubcup/HomeController.groovy
+++ grails-app/controllers/pubcup/HomeController.groovy
@@ -23,16 +23,17 @@ class HomeController {
     }
 
     def near(Float lat1, Float long1, Float lat2, Float long2, String gameId ){
-        def locations = []
+        def locations
 
     	def box = [ [ lat1 , long1 ] , [ lat2 , long2 ] ]
     	
-    	if(latitude && longitude){
-	    	if(gameId)
-	    		locations = Location.findByLocationWithinBoxAndEventGameId(box, gameId)
-	    	else
-	    		locations = Location.findByLocationWithinBox(box)
-    	}
+    	if(gameId)
+    		locations = Location.findByLocationWithinBoxAndEventGameId(box, gameId)
+    	else
+    		locations = Location.findByLocationWithinBox(box)
+
+    	if(!locations)
+    		locations = []
 
     	render locations as JSON
 
